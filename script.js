de040e28fb1a82fe676301dafa8e33eb32eb35de
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-01-10 11:21:18 +0000

    Open a form to add new book, form does not submit yet

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// Initialize library array
 let myLibrary = [];
 
+let body = document.body;
+
+// Book constructor
 function Book(name, author) {
     this.name = name;
     this.author = author;
 }
 
+// Create new book
 function addBookToLibrary(nome, autor) {
     myLibrary.push(new Book(nome, autor));
 }
@@ -12,11 +17,16 @@ function addBookToLibrary(nome, autor) {
 addBookToLibrary('asdasd', 'joao');
 addBookToLibrary('maria sul', 'rui');
 addBookToLibrary('fado', 'camane');
+addBookToLibrary('s', 'camane');
+addBookToLibrary('s', 'camane');
+addBookToLibrary('s', 'camane');
 
 console.table(myLibrary);
 
+// Container for the book cards
 let libraryContainer = document.querySelector(".library");
 
+// Create a card for each of the books in the library(array)
 myLibrary.forEach(book => {
     let bookCard = document.createElement('div');
     bookCard.id = myLibrary.indexOf(book);
@@ -32,4 +42,68 @@ myLibrary.forEach(book => {
     bookCard.appendChild(authorName);
     libraryContainer.appendChild(bookCard);  
 }); 
+
+// Open 'new book' form
+let newBookButton = document.querySelector(".newBook");
+newBookButton.addEventListener("click", function () {
+
+    // Display Form
+    let formContainer = document.querySelector('.formContainer')
+    formContainer.style.display = 'flex';
+    
+    // Title of the book
+    let titleTextBox = document.createElement('input');
+    titleTextBox.name = "title";
+    titleTextBox.id = "title";
+
+    let labelTitleTextBox = document.createElement('label');
+    labelTitleTextBox.textContent = "Title:";
+    labelTitleTextBox.setAttribute("for", "title");
+
+    formContainer.appendChild(labelTitleTextBox);
+    formContainer.appendChild(titleTextBox);  
+
+    // Author of the book
+    let authorTextBox = document.createElement('input');
+    authorTextBox.name = "author";
+    authorTextBox.id = "author";
+
+    let labelAuthorTextBox = document.createElement('label');
+    labelAuthorTextBox.textContent = "Author:";
+    labelAuthorTextBox.setAttribute("for", "author");
+
+    formContainer.appendChild(labelAuthorTextBox);
+    formContainer.appendChild(authorTextBox);
+
+    // How many pages
+    let pagesTextBox = document.createElement('input');
+    pagesTextBox.name = "pages";
+    pagesTextBox.id = "pages";
+
+    let labelPagesTextBox = document.createElement('label');
+    labelPagesTextBox.textContent = "Pages:";
+    labelPagesTextBox.setAttribute("for", "pages");
+
+    formContainer.appendChild(labelPagesTextBox);
+    formContainer.appendChild(pagesTextBox);
+
+    // If book has been read
+    let readCheckBox = document.createElement('input');
+    readCheckBox.type = "checkbox";
+    readCheckBox.name = "read";
+    readCheckBox.id = "read";
+
+    let labelReadCheckbox = document.createElement('label');
+    labelReadCheckbox.textContent = "Read:";
+    labelReadCheckbox.setAttribute("for", "read");
+
+    formContainer.appendChild(labelReadCheckbox);
+    formContainer.appendChild(readCheckBox);
+
+    // Submit new book
+    let submitButton = document.createElement('button');
+    submitButton.textContent = "Add";
+
+    formContainer.appendChild(submitButton);
+});
 
